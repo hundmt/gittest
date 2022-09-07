99b3ffe28d9d4fd92a4345b5b80964ca71b08d87
yangyang <860978186@qq.com>
2022-09-07 17:51:46 +0800

    第一次提交

---
+++ master/src/main/java/org/example/main/TestMain.java
@@ -0,0 +1,14 @@
+package org.example.main;
+
+/**
+ * ClassName:TestMain
+ * Package:org.example.main
+ * Date:2022/9/7 17:49
+ * Description:
+ * Author:sun
+ */
+public class TestMain {
+	public void testMain(){
+		System.out.println("1234");
+	}
+}
